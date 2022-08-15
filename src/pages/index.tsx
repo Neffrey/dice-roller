caa9593370a10da0ffc50c8c118b065d59b7de4b
Neffrey <neffreyl@gmail.com>
2022-08-15 05:23:42 -0500

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,28 +10,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>
-          Create <span>T3</span> App
-        </h1>
+        <h1>Dice Roller</h1>
 
         <div>
-          <h3>This stack uses:</h3>
-          <ul>
-            <li>
-              <a href="https://nextjs.org" target="_blank" rel="noreferrer">
-                Next.js
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://typescriptlang.org"
-                target="_blank"
-                rel="noreferrer"
-              >
-                TypeScript
-              </a>
-            </li>
-          </ul>
+          <h3>Hi mom</h3>
         </div>
       </div>
     </>
